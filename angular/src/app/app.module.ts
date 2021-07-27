d3e4e5d96e178e6186edc91c636ad2075e39f2b8
tjsk002 <tjsk002@naver.com>
2021-07-27 14:25:29 +0900

    깃 업로드 테스트

--- angular/src/app/app.module.ts
+++ angular/src/app/app.module.ts
@@ -1,31 +1,33 @@
-import { BrowserModule } from '@angular/platform-browser';
-import { NgModule } from '@angular/core';
+import {BrowserModule} from '@angular/platform-browser';
+import {NgModule} from '@angular/core';
 
-import { AppRoutingModule } from './app-routing.module';
-import { AppComponent } from './app.component';
+import {AppRoutingModule} from './app-routing.module';
+import {AppComponent} from './app.component';
 import {HeaderComponent} from './page/common/header/header.component';
-import { MainComponent } from './page/main/main.component';
-import { FooterComponent } from './page/common/footer/footer.component';
+import {MainComponent} from './page/main/main.component';
+import {FooterComponent} from './page/common/footer/footer.component';
 import {SwiperModule} from 'swiper/angular';
-import { ProjectComponent } from './page/project/project.component';
-import { IntroduceComponent } from './page/introduce/introduce.component';
-import { RegIntroduceComponent } from './page/reg/reg-introduce/reg-introduce.component';
+import {ProjectComponent} from './page/project/project.component';
+import {IntroduceComponent} from './page/introduce/introduce.component';
+import {RegIntroduceComponent} from './page/reg/reg-introduce/reg-introduce.component';
 import {FormsModule} from '@angular/forms';
-import { WebviewComponent } from './page/modal/webview/webview.component';
+import {WebviewComponent} from './page/modal/webview/webview.component';
 import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
+import {HistoryComponent} from './page/history/history.component';
 
 @NgModule({
-    declarations: [
-        AppComponent,
-        HeaderComponent,
-        HeaderComponent,
-        MainComponent,
-        FooterComponent,
-        ProjectComponent,
-        IntroduceComponent,
-        RegIntroduceComponent,
-        WebviewComponent
-    ],
+  declarations: [
+    AppComponent,
+    HeaderComponent,
+    HeaderComponent,
+    MainComponent,
+    FooterComponent,
+    ProjectComponent,
+    IntroduceComponent,
+    RegIntroduceComponent,
+    WebviewComponent,
+    HistoryComponent
+  ],
   imports: [
     BrowserModule,
     AppRoutingModule,
@@ -37,4 +39,5 @@ import {NgbModule} from '@ng-bootstrap/ng-bootstrap';
   bootstrap: [AppComponent],
   entryComponents: [WebviewComponent]
 })
-export class AppModule { }
+export class AppModule {
+}
